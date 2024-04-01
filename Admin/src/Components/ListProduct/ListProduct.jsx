122c0116dbd8605d99e6cccd208904c35e14e0da
Aminek <Kourou.amine@gmail.com>
2024-04-01 01:18:32 +0000

    admin panel 2

---
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ListProduct.css'
+
+function ListProduct(props) {
+    return (
+        <div className='ListProduct'>
+            
+        </div>
+    );
+}
+
+export default ListProduct;
